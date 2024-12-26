a45ce3af3a13139a0a06252f4a6856693fcd4770
SFboina <boinasoifaou@gmail.com>
2024-12-26 12:55:11 +0100

    modifation btn modifier

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -319,10 +319,17 @@ if (localStorage.token) {
   document.querySelector('.log').addEventListener('click', () => {
     localStorage.clear();
     window.location.href = 'connexion.html';
+
+    // Affiche l'icône et le bouton "modifier" 
+    document.querySelector('.fa-regular.fa-pen-to-square').style.display = 'inline'; 
+    document.querySelector('.open-modal').style.display = 'inline';
   });
 } else {
   // Cache la barre
   document.querySelector('.bar-side').style.display = 'none';
+  // Cache l'icône et le bouton "modifier" 
+  document.querySelector('.fa-regular.fa-pen-to-square').style.display = 'none'; 
+  document.querySelector('.open-modal').style.display = 'none'
 }
 
 const inputImage = document.getElementById("image");
